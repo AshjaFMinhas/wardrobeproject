6b59acbee49dadb74115f6d3d287501f459a63ad
ashjaminhas <38127720+ashjaminhas@users.noreply.github.com>
2018-04-24 12:41:51 +0200

    test

---
+++ src/main/java/com/capgemini/wardrobeproject/WardrobeprojectApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.wardrobeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardrobeprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardrobeprojectApplication.class, args);
+	}
+}
